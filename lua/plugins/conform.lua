6ecb1300cf44ce2abe641e365aaef126885b9936
Vexruna <vexruna@gmail.com>
2026-09-03 23:36:54 +0300

    minor fixes, biome is now the formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,9 +1,7 @@
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
-		-- Conform will run the first available formatter
-		javascript = { "prettierd", "prettier", stop_after_first = true },
+		javascript = { "biome" },
 	},
 	format_on_save = {
 		-- These options will be passed to conform.format()
